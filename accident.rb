42aea0efa77e86a745ebd043509701df21076609
miyazawa220618 <m.yukika341@gmail.com>
2022-11-17 21:30:09 +0900

    設問2の回答
    ターミナルで「ruby accident.rb」を実行し、タイムアウト回数を入力すると、出力日時のファイル名のlogが出力されます。

--- accident.rb
+++ accident.rb
@@ -2,25 +2,34 @@ require 'logger'
 require 'time'
 require 'date'
 
-filename = DateTime.now.strftime('%Y%m%d%H%M%S')
-logger = Logger.new("./#{filename}.log")
-logger.datetime_format = ''
+def ping_accident(log, time, number)
+  filename = DateTime.now.strftime('%Y%m%d%H%M%S')
+  logger = Logger.new("./#{filename}.log")
+  logger.datetime_format = ''
+
+  (number.length - time).times do |i|
+    if (number[time + i - 1] - number[i]) == time - 1
+      dt = Time.parse(log[number[time + i - 1]][0]) + log[number[time + i - 1]][2].to_i;
+      accident = "#{log[number[i]][1]}, #{log[number[i]][0]} - #{dt.strftime("%Y%m%d%H%M%S")}"
+      logger.error(accident)
+    end
+  end
+end
 
 log = []
-a = []
+number = []
+last_line = 0
 
 File.open('ping.log'){|f|
   f.each_line{|line|
-    error = line.chomp.split(",")
-    log.push error
-    if error[2] == "-"
-      a.push f.lineno
+    a = line.chomp.split(",")
+    log.push a
+    if a[2] == "-"
+      number.push f.lineno
     end
   }
 }
 
-a.each do |i|
-  dt = Time.parse(log[i][0]) + log[i][2].to_i;
-  accident = "#{log[i - 1][1]}, #{log[i - 1][0]} - #{dt.strftime("%Y%m%d%H%M%S")}"
-  logger.error(accident)
-end
+puts 'サーバ故障のタイムアウト回数を指定してください'
+time = gets.to_i
+ping_accident(log, time, number)
